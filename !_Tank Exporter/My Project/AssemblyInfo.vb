4c600215e0b6388229ed6d6437078d1dabbca2d7
mikeoverbay <thecooltool@hotmail.com>
2024-10-09 21:09:46 -0400

    Version 153

--- !_Tank Exporter/My Project/AssemblyInfo.vb
+++ !_Tank Exporter/My Project/AssemblyInfo.vb
@@ -32,6 +32,6 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.0.152")>
-<Assembly: AssemblyFileVersion("1.0.0.152")>
+<Assembly: AssemblyVersion("1.0.0.153")>
+<Assembly: AssemblyFileVersion("1.0.0.153")>
 <Assembly: NeutralResourcesLanguage("")>
